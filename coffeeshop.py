8858a7177f1c483806cf237849aad171d0e6f7db
Tiger03z <oliveraarflotn@gmail.com>
2026-09-22 11:36:06 +0200

    Gjør om printsene til funkjon for å eklere plasere de og sånt

--- coffeeshop.py
+++ coffeeshop.py
@@ -1,21 +1,28 @@
 #Kilde: The Coffee Shop Price Calculator - www.101computing.net/the-coffee-shop-price-calculator
-#Printer ut al informasjon som skal sendes i begynelsen
-print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
-print("+                               +")
-print("+         The Coffee Shop       +")
-print("+              Welcome          +")
-print("+                               +")
-print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
-print("")
-print("We serve the following coffees:")
-print(" > Espresso      £2.50")
-print(" > Americano     £3.00")
-print(" > Latte         £2.50")
-print(" > Cappuccino    £3.00")
-print(" > Macchiato     £2.50")
-print(" > Mocha         £3.50")
-print(" > Flat White    £2.50")
-print("----------------------------")
+
+# funksjoner som printer ut al informasjon som skal sendes i begynelsen
+def coffeeMeny ():
+   print("We serve the following coffees:")
+   print("----------------------------")
+   print(" > Espresso      £2.50")
+   print(" > Americano     £3.00")
+   print(" > Latte         £2.50")
+   print(" > Cappuccino    £3.00")
+   print(" > Macchiato     £2.50")
+   print(" > Mocha         £3.50")
+   print(" > Flat White    £2.50")
+   print("----------------------------")
+def welcome():
+   print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+   print("+                                 +")
+   print("+         The Coffee Shop         +")
+   print("+             Welcome             +")
+   print("+                                 +")
+   print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+   print("")
+
+welcome()
+coffeeMeny()
 
 price = 0
 coffee = input("What type of coffee would you like? ").title()
