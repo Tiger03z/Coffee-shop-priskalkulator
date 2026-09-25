d1eca2ab7a0362fc878bf36478c5772db626f3b6
tiger03z <oliveraarflotn@gmail.com>
2026-09-25 15:12:46 +0200

    jeg har gjort så my forskjelig, omplasing, ordbok som blir større når du skrifer et stort tall, og blir addet når det trengs, løkker og funkjoner

--- coffeeshop.py
+++ coffeeshop.py
@@ -2,20 +2,21 @@
 
 "variabler"
 price = 0
-antallKoper = 0
 kaffer = []
+sm = 0
 
 
-
-
-
-
-
-def spørOmKopper():
-   return int(input("How manny cups do you want? "))
-
+ 
 
 # funksjoner som printer ut al informasjon som skal sendes i begynelsen
+def welcome():
+   print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+   print("+                                 +")
+   print("+         The Coffee Shop         +")
+   print("+             Welcome             +")
+   print("+                                 +")
+   print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+   print("")
 def coffeeMeny ():
    print("We serve the following coffees:")
    print("----------------------------")
@@ -27,31 +28,46 @@ def coffeeMeny ():
    print(" > Mocha         £3.50")
    print(" > Flat White    £2.50")
    print("----------------------------")
-def welcome():
-   print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
-   print("+                                 +")
-   print("+         The Coffee Shop         +")
-   print("+             Welcome             +")
-   print("+                                 +")
-   print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
-   print("")
+def kopperAdd ():
+   for x in range(antallKoper):
+      kaffer.append({f"{x}smak": "", "size": "", "tillbehør": ""})
+########
+
+
 
 welcome()
-antallKoper = spørOmKopper()
+
+antallKoper = int(input("How manny cups do you want? "))
+kopperAdd()
+
+print(kaffer)
+
 coffeeMeny()
 
-for x in antallKoper:
-   kaffer.append = input("What coffee do you want? ")
 
 
-coffee = input("What type of coffee would you like? ").title()
+
+while antallKoper > 0 :
+   kaffe = str(input("What coffee do you want? "))
+   antallKoper -= 1
+   kaffer[sm][f"{sm}smak"] = kaffe
+   sm += 1
+print(kaffer)
+
+
+#Complete the code here...
+print("----------------------------")
+print("Total Cost: £" + str(price))
+
+
+
+
+#not using prob
+"""coffee = input("What type of coffee would you like? ").title()
 if coffee=="Espresso":
    price = price + 2.50
 elif coffee=="Americano":
    price = price + 3
 elif coffee=="Latte":
    price = price + 2.50
-
-#Complete the code here...
-print("----------------------------")
-print("Total Cost: £" + str(price))
+"""
